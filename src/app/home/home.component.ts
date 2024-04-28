a055ca185445da8067e47c5a7e0953fe85c0dbf1
KiraSeletska <seletskaya.kira@gmail.com>
2024-04-28 16:10:32 +0200

    add: home animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.animateText();
+  }
+
+  animateText(): void {
+    gsap.from(['h1', 'h2', '.skills', '.text'], {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      ease: 'power3.out',
+      stagger: 0.3 
+    });
+
+  }
 
 }
